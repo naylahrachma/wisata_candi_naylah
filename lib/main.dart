570975381c65b61f99d47294314705fdf5377b54
naylahrachma <naylahsari556@mhs.mdp.ac.id>
2023-10-26 11:23:22 +0700

    kode awal sebelum selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,16 +19,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      // home: DetailScreen(candi: candiList[0]),
+      home : ProfileScreen (),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget{
-  const DetailScreen ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
